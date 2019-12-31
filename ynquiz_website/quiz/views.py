72221507ec7ea55505b5f20fd19af0f0db09b089
VincentPetrini <vincent.petrini@isen.yncrea.fr>
2019-12-31 18:32:58 +0100

    Affichage question et reponse à partir du model

--- ynquiz_website/quiz/views.py
+++ ynquiz_website/quiz/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from quiz.models import Answer
+from quiz.models import Question
 
 def home(request):
     return render(request, 'quiz/home.html', locals())
 
 def play(request):
-    return render(request, 'quiz/play.html', locals())
+    answers = Answer.objects.filter(question__id=1)
+    question = Question.objects.filter(id=1)
+    return render(request, 'quiz/play.html', {'reponses': answers, 'question': question[0]})
 
 def ranking(request):
     return render(request, 'quiz/ranking.html', locals())
