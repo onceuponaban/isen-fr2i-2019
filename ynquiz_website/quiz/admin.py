e3eba3398eda6d026404dddb353237103e96fc60
VincentPetrini <vincent.petrini@isen.yncrea.fr>
2020-02-03 15:13:50 +0100

    Ajout système d'administration Question/Réponse

--- ynquiz_website/quiz/admin.py
+++ ynquiz_website/quiz/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import Question, Answer
 
 admin.site.register(User, UserAdmin)
+
+class AnswersInline(admin.StackedInline):
+    model = Answer
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        AnswersInline,
+    ]
+
+
+#admin.site.register(Question)
+#admin.site.register(Answer)
--- ynquiz_website/quiz/models.py
+++ ynquiz_website/quiz/models.py
@@ -11,8 +11,20 @@ class Question (models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     question_content = models.CharField(max_length=500)
 
+    class Meta:
+        verbose_name = "question"
+
+    def __str__(self):
+        return self.question_content
+
 class Answer (models.Model):
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=100)
     is_correct = models.BooleanField('correct answer', default=False)
+
+    class Meta:
+        verbose_name = "réponse"
+
+    def __str__(self):
+        return self.answer_text
     
